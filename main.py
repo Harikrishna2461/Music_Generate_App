8d96f7b075ba99ddf606e6cf0c9d47cd5c3623e3
Hari Krishna Dhamodaran <67218513+Harikrishna2461@users.noreply.github.com>
2023-11-12 12:39:47 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,95 +1,100 @@
-#This file can be run on replit,local or any other IDEs,but make sure Meta's 
-#'audiocraft' library folder containing all the models and other required files is in 
-#the same directory as the main.py file.
-import base64
-import os
-import numpy as np
-import streamlit as st
-import torch
-import torchaudio
 from audiocraft.models import MusicGen
+import streamlit as st 
+import torch 
+import torchaudio
+import os 
+import numpy as np
+import base64
 
-#Loading the Pretrained MusicGen Model from the audiocraft library of Meta,which takes 
-#text inputs and Generate a Music File/Media according to the user text input.
-#We are also caching the model so we dont need to load the model again and again during 
-#runtime and reduce costs
-def Gen_AI_Model():
-  Gen_AI_Model = MusicGen.get_pretrained("facebook/musicgen-small")
-  return Gen_AI_Model
+@st.cache_resource
+def load_model():
+    model = MusicGen.get_pretrained('facebook/musicgen-small')
+    return model
 
-#to generate the numerical tensors of the generate music audio.
-def generate_music_tensors(description,duration:int):
-  model = Gen_AI_Model()
-  #Setting the Hyperparameters,refer to the MusicGen Model's Github page to see what 
-  #exactly these parameters signify.Here 'use_sampling' is used to control whether the 
-  #model samples from existing music or creates completely new music,top_k refers to 
-  #top k number of musics which match the text description to sample from and duration
-  #refers to the duration of the music generated.
-  
-  model.set_generation_params(use_sampling=False,top_k=250,duration=duration)
-  generated_music = model.generate(descriptions=[description],progress=True,return_tokens=True)
-  return generated_music[0]
+def generate_music_tensors(description, duration: int):
+    print("Description: ", description)
+    print("Duration: ", duration)
+    model = load_model()
 
-#To load the generated_music_tensors into a music we use the following function.
-#This Renders an audio player for the given audio samples and saves them to a local 
-#directory.
-  
-#Arguements:
-    #samples (torch.Tensor): a Tensor of decoded audio samples
-        #with shapes [B, C, T] or [C, T]
-    #sample_rate (int): sample rate audio should be displayed with.
-    #save_path (str): path to the directory where audio should be saved.
+    model.set_generation_params(
+        use_sampling=True,
+        top_k=250,
+        duration=duration
+    )
 
-def load_music(samples: torch.Tensor):
-  print("Samples (inside function): ", samples)
-  sample_rate = 32000
-  save_path = "audio_output/"
-  assert samples.dim() == 2 or samples.dim() == 3
+    output = model.generate(
+        descriptions=[description],
+        progress=True,
+        return_tokens=True
+    )
 
-  samples = samples.detach().cpu()
-  if samples.dim() == 2:
-      samples = samples[None, ...]
+    return output[0]
 
-  for idx, audio in enumerate(samples):
-      audio_path = os.path.join(save_path, f"audio_{idx}.wav")
-      torchaudio.save(audio_path, audio, sample_rate)
 
-def get_binary_file(bin_file, file_label='File'):
+def save_audio(samples: torch.Tensor):
+    """Renders an audio player for the given audio samples and saves them to a local directory.
+
+    Args:
+        samples (torch.Tensor): a Tensor of decoded audio samples
+            with shapes [B, C, T] or [C, T]
+        sample_rate (int): sample rate audio should be displayed with.
+        save_path (str): path to the directory where audio should be saved.
+    """
+
+    print("Samples (inside function): ", samples)
+    sample_rate = 32000
+    save_path = "audio_output/"
+    assert samples.dim() == 2 or samples.dim() == 3
+
+    samples = samples.detach().cpu()
+    if samples.dim() == 2:
+        samples = samples[None, ...]
+
+    for idx, audio in enumerate(samples):
+        audio_path = os.path.join(save_path, f"audio_{idx}.wav")
+        torchaudio.save(audio_path, audio, sample_rate)
+
+def get_binary_file_downloader_html(bin_file, file_label='File'):
     with open(bin_file, 'rb') as f:
         data = f.read()
     bin_str = base64.b64encode(data).decode()
     href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{os.path.basename(bin_file)}">Download {file_label}</a>'
     return href
 
-#Using a github emoji for music as the page icon.
-st.set_page_config(page_title="Music Generator",page_icon=":musical_note:")
+st.set_page_config(
+    page_icon= "musical_note",
+    page_title= "Music Gen"
+)
 
-#Instructing the user what to do
-st.write("Give text input to get the desired Music you want")
+def main():
 
-text_input = st.text_area("Give your Input")
-#Setting the time slider to generate music for that many seconds as specified by the 
-#user with minimum = 5 seconds,maximum = 360 seconds and default = 20 seconds
-time_slider = st.slider("Give the duration of the music (in seconds)",5,20,360)
+    st.title("Text to Music Generator🎵")
 
-if text_input and time_slider : #When these fileds are not empty.
-  st.json(
-           {
-           "description":text_input,
-           "selected time duration (in seconds):":time_slider
-          }
-        )
-  st.subheader("Generated Music")
-  generated_music_tensor = generate_music_tensors(text_input, time_slider)
-  print("Musci Tensors: ", generated_music_tensor)
-  generated_music_file = load_music(generated_music_tensor)
-  music_filepath = 'audio_output/audio_0.wav'
-  audio_file = open(music_filepath, 'rb')
-  audio_bytes = audio_file.read()
-  st.audio(audio_bytes)
-  st.markdown(get_binary_file(music_filepath, 'Audio'), unsafe_allow_html=True)
+    with st.expander("See explanation"):
+        st.write("Music Generator app built using Meta's Audiocraft library. We are using Music Gen Small model.")
 
-#The End.
-  
-   
+    text_area = st.text_area("Enter your description.......")
+    time_slider = st.slider("Select time duration (In Seconds)", 0, 20, 10)
+
+    if text_area and time_slider:
+        st.json({
+            'Your Description': text_area,
+            'Selected Time Duration (in Seconds)': time_slider
+        })
+
+        st.subheader("Generated Music")
+        music_tensors = generate_music_tensors(text_area, time_slider)
+        print("Musci Tensors: ", music_tensors)
+        save_music_file = save_audio(music_tensors)
+        audio_filepath = 'audio_output/audio_0.wav'
+        audio_file = open(audio_filepath, 'rb')
+        audio_bytes = audio_file.read()
+        st.audio(audio_bytes)
+        st.markdown(get_binary_file_downloader_html(audio_filepath, 'Audio'), unsafe_allow_html=True)
+
+
+if __name__ == "__main__":
+    main()
+    
+
 
